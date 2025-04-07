9ec684f7c74c8a70bd1f5febc9f7bd458eddb33e
Alymuhammad Bijani <a.bijani@wustl.edu>
2025-04-06 20:17:15 -0500

    user tests updated based on new functionality

--- src/bankapp/User.java
+++ src/bankapp/User.java
@@ -1,7 +1,7 @@
 package bankapp;
 import java.util.*;
 
-class User {
+public class User {
     private String username;
     private String password;
     private Map<String, BankAccount> accounts = new HashMap<>();
